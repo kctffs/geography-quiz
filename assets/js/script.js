f72177c50827daed2af1272a9de4a6f07b53d462
Kieran Cattermole <kctffs@gmail.com>
2024-04-07 00:28:50 +0000

    created 10 questions in js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,88 @@
 const question = document.getElementById('question');
-const choices = document.getElementById('choices');
-const scoring = document.getElementById('score');
+const choices = document.getElementById('choices-text');
+const scoreNum = document.getElementById('score');
+
+let currentQuestion = {};
+
+let questions = [
+    {
+        question: 'What country outline matches this flag?',
+        choice1: 'A',
+        choice2: 'B',
+        choice3: 'C',
+        choice4: 'D',
+        asnwer: 2,
+    },
+    {
+        question: 'What country outline matches this flag?',
+        choice1: 'A',
+        choice2: 'B',
+        choice3: 'C',
+        choice4: 'D',
+        asnwer: 1,
+    },
+    {
+        question: 'What country outline matches this flag?',
+        choice1: 'A',
+        choice2: 'B',
+        choice3: 'C',
+        choice4: 'D',
+        asnwer: 2,
+    },
+    {
+        question: 'What country outline matches this flag?',
+        choice1: 'A',
+        choice2: 'B',
+        choice3: 'C',
+        choice4: 'D',
+        asnwer: 3,
+    },
+    {
+        question: 'What country outline matches this flag?',
+        choice1: 'A',
+        choice2: 'B',
+        choice3: 'C',
+        choice4: 'D',
+        asnwer: 4,
+    },
+    {
+        question: 'What country outline matches this flag?',
+        choice1: 'A',
+        choice2: 'B',
+        choice3: 'C',
+        choice4: 'D',
+        asnwer: 3,
+    },
+    {
+        question: 'What country outline matches this flag?',
+        choice1: 'A',
+        choice2: 'B',
+        choice3: 'C',
+        choice4: 'D',
+        asnwer: 1,
+    },
+    {
+        question: 'What country outline matches this flag?',
+        choice1: 'A',
+        choice2: 'B',
+        choice3: 'C',
+        choice4: 'D',
+        asnwer: 1,
+    },
+    {
+        question: 'What country outline matches this flag?',
+        choice1: 'A',
+        choice2: 'B',
+        choice3: 'C',
+        choice4: 'D',
+        asnwer: 2,
+    },
+    {
+        question: 'What country outline matches this flag?',
+        choice1: 'A',
+        choice2: 'B',
+        choice3: 'C',
+        choice4: 'D',
+        asnwer: 4,
+    }
+]
